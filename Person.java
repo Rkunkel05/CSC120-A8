b1c07173221b7008eb70578812ee9222e416c5ea
Rachel <rkunkel@smith.edu>
2024-04-05 15:19:00 -0400

    idk ):

--- Person.java
+++ Person.java
@@ -1,19 +1,12 @@
-abstract class Person {
-
+public class Person {
     // Location
     protected int x, y;
     protected String direction;
 
     // Qualities
     protected String name;
-    protected Boolean moving; 
-    protected Boolean flying;
-
-     // Need to make a person to do the actions!
-    public void Person() {
-        moving = false;
-        flying = false;
-    }
+    protected Boolean moving = false;
+    protected Boolean flying = false;
 
     // Getters
     public int getX() {
@@ -36,4 +29,17 @@ abstract class Person {
         return this.getClass().getCanonicalName();
     }
 
+    public boolean walk(String direction) {
+        if (moving = false) {
+            System.out.println("You are now moving in " + direction);
+            return true;
+        } else {
+            throw new RuntimeException("You are already moving!");
+        }
+    }
+
+    public static void main(String[] args) {
+        Person me = new Person();
+        System.out.println(me);
+    }
 }
